5c34e75b3480d2e31eda116a4d85c2e2fa694ced
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-02-23 00:01:38 -0300

    removeu import - tick

--- src/br/com/vini/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/vini/gerenciador/servlet/NovaEmpresaServlet.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
