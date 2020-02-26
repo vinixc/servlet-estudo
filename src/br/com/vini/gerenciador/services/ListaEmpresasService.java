7388ed6df70feda3db66198d98a7314fce1a7aa4
Vinicius <vinicius.tecbcc@gmail.com>
2020-02-26 17:40:14 -0300

    servico que retorna tando um xml como um json

--- src/br/com/vini/gerenciador/services/ListaEmpresasService.java
+++ src/br/com/vini/gerenciador/services/ListaEmpresasService.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.vini.gerenciador.entities.Banco;
@@ -22,20 +23,28 @@ public class ListaEmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		List<Empresa> empresas = Banco.getEmpresas();
 		
-		XStream xstream = new XStream();
-		xstream.alias("empresa", Empresa.class);
-		String xml = xstream.toXML(empresas);
-		
-		response.setContentType("application/xml");
-		response.getWriter().print(xml);
-		
-//		Gson gson = new Gson();
-//		String json = gson.toJson(empresas);
-//		
-//		response.setContentType("application/json");
-//		response.getWriter().print(json);
+		String valor = request.getHeader("Accept");
+		System.out.println(valor);
+		 
+		if(valor.endsWith("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+			
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+		}else if(valor.endsWith("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		}else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message': 'no content'}");
+		}
 	}
-
 }
